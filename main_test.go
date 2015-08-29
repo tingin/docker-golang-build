f7c4055dbc0d026bd6340ba36e0f48b90d650558
ting <bail@uway.cn>
2015-08-29 18:37:56 +0800

    add chrome test

--- main_test.go
+++ main_test.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"github.com/tingin/webdriver"
+	"log"
 	"testing"
+	"time"
 )
 
-func TestStub(t *testing.T) {
-
+func TestChrome(t *testing.T) {
+	chromeDriver := webdriver.NewChromeDriver("chromedriver")
+	err := chromeDriver.Start()
+	if err != nil {
+		log.Println(err)
+	}
+	desired := webdriver.Capabilities{"Platform": "Linux"}
+	required := webdriver.Capabilities{}
+	session, err := chromeDriver.NewSession(desired, required)
+	if err != nil {
+		log.Println(err)
+	}
+	err = session.Url("http://golang.org")
+	if err != nil {
+		log.Println(err)
+	}
+	time.Sleep(10 * time.Second)
+	session.Delete()
+	chromeDriver.Stop()
 }
